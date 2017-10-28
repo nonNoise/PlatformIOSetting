a6a42be9254f7278d0d74f3804aa76df35dadb03
nonNoise <hokusin02@gmail.com>
2017-10-28 23:34:19 +0900

    add

--- Sphinx/conf.py
+++ Sphinx/conf.py
@@ -96,7 +96,7 @@ html_theme_options = {
     'header_inverse': False,
     'relbar_inverse': False,
     'inner_theme': True,
-    'inner_theme_name': 'bootswatch-cosmo',
+    'inner_theme_name': 'bootswatch-slate',
 }
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
